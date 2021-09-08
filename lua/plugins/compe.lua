94ec6f3cfd77c0854ca8e1bd23ea43d99bcf2ba0
blinnk <willfromspace@gmail.com>
2021-09-07 21:09:24 -0700

    fixed compe + luasnip

--- lua/plugins/compe.lua
+++ lua/plugins/compe.lua
@@ -1,14 +1,15 @@
--- vim.o.completeopt = "menu,menuone,noselect"
-vim.o.completeopt = "longest"
+vim.o.completeopt = "menuone,noselect"
+-- vim.o.completeopt = "longest"
+local luasnip = require('luasnip')
 
-require'cmp'.setup ({
+require'compe'.setup({
     enabled = true,
     autocomplete = true,
     debug = false,
     min_length = 1,
     preselect = "enable",
-    throttle_time = 80,
-    source_timeout = 200,
+    throttle_time = 100,
+    -- source_timeout = 200,
     incomplete_delay = 400,
     max_abbr_width = 100,
     max_kind_width = 100,
@@ -16,22 +17,24 @@ require'cmp'.setup ({
     documentation = true,
 
     source = {
-      path = { kind = "   (Path)" },
-      buffer = { kind = "   (Buffer)" },
-      calc = { kind = "   (Calc)" },
-      vsnip = { kind = "   (Snippet)" },
-      nvim_lsp = { kind = "   (LSP)" },
       nvim_lua = true,
-      tags = true,
       snippets_nvim = true,
       vim_lsp = true,
       ultisnips = true,
-      -- emmet_ls = true,
+      luasnip = true,
       vim_snippets = true,
-      treesitter = true,
       friendly = true,
-      emoji = { kind = " ﲃ  (Emoji)", filetypes = { "markdown", "text", "htmldjango" } },
-      spell = { kind = "   (Spell)", filetypes = { "markdown", "text", "JSON"} },
+      emoji = true,
+      path = { kind = "   (Path)" },
+      buffer = { kind = "   (Buffer)" },
+      vsnip = { kind = "   (Snippet)" },
+      nvim_lsp = { kind = "   (LSP)" },
+      spell = { kind = "   (Spell)", filetypes = { "markdown", "text", "JSON", "HTML"} },
+      -- tags = true,
+      -- calc = { kind = "   (Calc)" },
+      -- treesitter = true,
+      -- emoji = { kind = " ﲃ  (Emoji)", filetypes = { "markdown", "text", "htmldjango" } },
+      -- emmet_ls = true,
 
 
       -- vim_dadbod_completion = false,
@@ -88,6 +91,33 @@ vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()",     { expr = true 
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
 
+_G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif luasnip and luasnip.expand_or_jumpable() then
+        return t "<Plug>luasnip-expand-or-jump"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn['compe#complete']()
+    end
+end
+_G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif luasnip and luasnip.jumpable(-1) then
+        return t "<Plug>luasnip-jump-prev"
+    else
+        return t "<S-Tab>"
+    end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
+vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
 
 
 
